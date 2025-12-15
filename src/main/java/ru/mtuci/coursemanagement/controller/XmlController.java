be06df9902618602a7474a9a7ee4bcafe2ba2816
GeorgeFirsov <goga.fir05@gmail.com>
2025-12-15 16:06:00 +0300

    fix XXE - убираем зависимость от dom4j

--- src/main/java/ru/mtuci/coursemanagement/controller/XmlController.java
+++ src/main/java/ru/mtuci/coursemanagement/controller/XmlController.java
@@ -1,8 +1,6 @@
 package ru.mtuci.coursemanagement.controller;
 
 import jakarta.servlet.http.HttpSession;
-import org.dom4j.Document;
-import org.dom4j.io.SAXReader;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,12 +8,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.mtuci.coursemanagement.util.CsrfUtil;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+
 import java.io.StringReader;
 
 @RestController
 public class XmlController {
 
-    @PostMapping(value = "/api/xml/parse", consumes = {MediaType.TEXT_XML_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @PostMapping(
+            value = "/api/xml/parse",
+            consumes = {MediaType.TEXT_XML_VALUE, MediaType.APPLICATION_XML_VALUE}
+    )
     public String parse(@RequestBody String xml,
                         @RequestParam String csrf,
                         HttpSession session) throws Exception {
@@ -24,8 +30,16 @@ public class XmlController {
             return "CSRF error";
         }
 
-        SAXReader reader = new SAXReader();
-        Document doc = reader.read(new StringReader(xml));
-        return doc.getRootElement().getText();
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+        factory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+        factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+        factory.setXIncludeAware(false);
+        factory.setExpandEntityReferences(false);
+
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(new InputSource(new StringReader(xml)));
+
+        return doc.getDocumentElement().getTextContent();
     }
 }
