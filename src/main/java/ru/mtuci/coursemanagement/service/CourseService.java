c7323f20b41d960c897b339fea09be189c494a57
GeorgeFirsov <goga.fir05@gmail.com>
2025-12-15 15:40:24 +0300

    fix SQL injection 2

--- src/main/java/ru/mtuci/coursemanagement/service/CourseService.java
+++ src/main/java/ru/mtuci/coursemanagement/service/CourseService.java
@@ -33,12 +33,16 @@ public class CourseService {
 
     public List<Course> searchByTitle(String title) {
         String sql = "SELECT id, title, description, teacher_id FROM courses WHERE title = ?";
-        RowMapper<Course> rm = (rs, i) -> new Course(
-                rs.getLong("id"),
-                rs.getString("title"),
-                rs.getString("description"),
-                rs.getLong("teacher_id")
-        );
+
+        RowMapper<Course> rm = (rs, i) -> {
+            Course c = new Course();
+            c.setId(rs.getLong("id"));
+            c.setTitle(rs.getString("title"));
+            c.setDescription(rs.getString("description"));
+            c.setTeacherId(rs.getLong("teacher_id"));
+            return c;
+        };
+
         return jdbc.query(sql, rm, title);
     }
 }
